26f255f7849e9671822802b8717aafb138ff613b
Oana Ionescu <oanitza12@gmail.com>
2018-11-27 21:57:27 +0100

    Huffman Coder & Quantizer

--- at/aau/itec/emmt/jpeg/stud/HuffmanCoder.java
+++ at/aau/itec/emmt/jpeg/stud/HuffmanCoder.java
@@ -1,7 +1,11 @@
 package at.aau.itec.emmt.jpeg.stud;
 
+import java.util.ArrayList;
+
 import at.aau.itec.emmt.jpeg.impl.AbstractHuffmanCoder;
+import at.aau.itec.emmt.jpeg.impl.RunLevel;
 import at.aau.itec.emmt.jpeg.spec.BlockI;
+import at.aau.itec.emmt.jpeg.spec.EntropyCoderI;
 import at.aau.itec.emmt.jpeg.spec.RunLevelI;
 
 public class HuffmanCoder extends AbstractHuffmanCoder {
@@ -9,6 +13,36 @@ public class HuffmanCoder extends AbstractHuffmanCoder {
     @Override
     public RunLevelI[] runLengthEncode(BlockI quantBlock) {
 
-        throw new RuntimeException("Not yet implemented :-(");
+    	 ArrayList<RunLevel> temp = new ArrayList<>();
+    	 
+         int[] quantArray = new int[quantBlock.getData().length * quantBlock.getData()[0].length];
+
+         for (int y = 0; y < quantBlock.getData().length; y++) {
+             for (int x = 0; x < quantBlock.getData()[y].length; x++) {
+                 quantArray[quantBlock.getData().length * y + x] = quantBlock.getData()[y][x];
+             }
+         }
+
+         int[] zigZag = EntropyCoderI.ZIGZAG_ORDER;
+         int zeros = 0;
+
+         for (int i = 1; i < quantArray.length; i++) {
+             if (quantArray[zigZag[i]] == 0) {
+                 zeros++;
+             } else {
+                 temp.add(new RunLevel(zeros, quantArray[zigZag[i]]));
+                 zeros = 0;
+             }
+         }
+
+         if (zeros > 0) {
+             temp.add(new RunLevel(zeros, 0));
+         }
+
+         RunLevel[] out = new RunLevel[temp.size()];
+         temp.toArray(out);
+
+         return out;
+       
     }
 }
--- at/aau/itec/emmt/jpeg/stud/Quantizer.java
+++ at/aau/itec/emmt/jpeg/stud/Quantizer.java
@@ -1,5 +1,7 @@
 package at.aau.itec.emmt.jpeg.stud;
 
+import at.aau.itec.emmt.jpeg.impl.Block;
+import at.aau.itec.emmt.jpeg.impl.YUVImage;
 import at.aau.itec.emmt.jpeg.spec.BlockI;
 import at.aau.itec.emmt.jpeg.spec.DCTBlockI;
 import at.aau.itec.emmt.jpeg.spec.QuantizationI;
@@ -15,20 +17,69 @@ public class Quantizer implements QuantizationI {
     public Quantizer(int qualityFactor) {
         this.qualityFactor = qualityFactor;
     }
-
+   
     @Override
     public int[] getQuantumLuminance() {
-        return new int[0];
+    	int scalingFactor;
+    	
+    	 if (qualityFactor < 50) {
+            scalingFactor = 5000 / qualityFactor;
+         }else{
+          scalingFactor= 200 - 2 * qualityFactor;
+          }   	
+    	 
+    	  int[] out = new int[QUANTUM_LUMINANCE.length];
+
+          for (int i = 0; i < QUANTUM_LUMINANCE.length; i++) {
+             out[i] = Math.min(255, Math.max(1, (QUANTUM_LUMINANCE[i] * scalingFactor + 50) /100));
+            }
+       	 
+    	return out;
+       
     }
 
     @Override
     public int[] getQuantumChrominance() {
-        return new int[0];
+    	
+     int scalingFactor;
+    	
+   	 if (qualityFactor < 50) {
+   		 
+           scalingFactor = 5000 / qualityFactor;
+        }else{
+         scalingFactor= 200 - 2 * qualityFactor;
+         }   	
+   	 
+   	  int[] out = new int[QUANTUM_CHROMINANCE.length];
+
+         for (int i = 0; i < QUANTUM_CHROMINANCE.length; i++) {
+            out[i] = Math.min(255, Math.max(1, (QUANTUM_CHROMINANCE[i] * scalingFactor + 50) /100));
+           }
+      	 
+   	return out;
+      
     }
 
     @Override
     public BlockI quantizeBlock(DCTBlockI dctBlock, int compType) {
-        return null;
+      
+    	 int[][] out = new int[dctBlock.getData().length][dctBlock.getData()[0].length];
+    	 
+    	 int[] quantum;
+    	 if(compType == YUVImage.Y_COMP) {
+    		 
+            quantum=getQuantumLuminance();		 
+    	 }else{
+    		 
+    		 quantum = getQuantumChrominance();
+    	 }
+    	
+         for (int y = 0; y < dctBlock.getData().length; y++) {
+             for (int x = 0; x < dctBlock.getData()[y].length; x++) {
+                 out[y][x] = (int) Math.round(dctBlock.getData()[y][x] / quantum[y * dctBlock.getData().length + x]);
+             }
+         }
+         return new Block(out);
     }
 
     @Override
